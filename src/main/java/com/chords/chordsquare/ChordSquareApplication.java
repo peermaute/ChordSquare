b1fa3d05af494993cfdf95afa5ac74ba0bb2e803
Peer Maute <peer.maute@haw-hamburg.de>
2021-12-03 09:41:09 +0100

    init commit

---
+++ src/main/java/com/chords/chordsquare/ChordSquareApplication.java
@@ -0,0 +1,13 @@
+package com.chords.chordsquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChordSquareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChordSquareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chords/chordsquare/ChordSquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chords.chordsquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChordSquareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
