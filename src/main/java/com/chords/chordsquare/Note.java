f4f78ed827a2d3c5d1a417f39a57483c30fa5827
Peer Maute <peer.maute@haw-hamburg.de>
2021-12-06 10:55:40 +0100

    enum for Note value added

--- src/main/java/com/chords/chordsquare/Note.java
+++ src/main/java/com/chords/chordsquare/Note.java
@@ -2,6 +2,7 @@ package com.chords.chordsquare;
 
 public class Note {
     private boolean altered;
+    private NoteValue noteValue;
     //higherKey(): when not altered, just alter unless it's e or b
     //lowerKey(): when not altered, just alter unless it's c or f
     //higherKey(): when altered, just deAlter
---
+++ src/main/java/com/chords/chordsquare/NoteValue.java
@@ -0,0 +1,15 @@
+package com.chords.chordsquare;
+
+public enum NoteValue {
+    C,D,E,F,G,A,B;
+
+    static public final NoteValue[] values = values();
+
+    public NoteValue previous(){
+        //Additional ( + values.length) added because modulo operator can return negative numbers in Java.
+        return values[(ordinal() -1 + values.length) % values.length];
+    }
+    public NoteValue next(){
+        return values[(ordinal() + 1) % values.length];
+    }
+}
