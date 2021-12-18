a725e2d7440feee5498f14fff132072f4022045f
Peer Maute <peer.maute@haw-hamburg.de>
2021-12-18 17:27:28 +0100

    SongService CRUD operations

--- src/main/java/com/chords/chordsquare/Controller.java
+++ src/main/java/com/chords/chordsquare/Controller.java
@@ -3,6 +3,7 @@ package com.chords.chordsquare;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,16 @@ public class Controller {
 
     @PostMapping(path = "/songs/new")
     public ResponseEntity<String> newSong(@RequestBody Map<String,String> map){
-        return songService.newSong(map);
+        if(songService.newSong(map)){
+            return new ResponseEntity<>("Creation successful", HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>("Creation failed, check logs", HttpStatus.BAD_REQUEST);
+    }
+    @DeleteMapping(path = "/songs/delete")
+    public ResponseEntity<String> deleteSong(@RequestBody String songId){
+        if(songService.deleteSong(songId)){
+            return new ResponseEntity<>("Deletion successful", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Deletion failed, check logs", HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/chords/chordsquare/SongService.java
+++ src/main/java/com/chords/chordsquare/SongService.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 @Service
@@ -16,10 +17,13 @@ public class SongService {
     @Autowired
     SongRepository songRepository;
 
-    public ResponseEntity<String> newSong(Map<String, String> map){
+    public boolean newSong(Map<String, String> map){
+        if(map == null){
+            throw new IllegalArgumentException("Argument must not be null");
+        }
         if (map.get("text") == null && map.get("name") == null) {
             System.out.println("POST Request FAILED: Either text or name has to be set");
-            return new ResponseEntity<>("Creation failed, either text or name has to be set", HttpStatus.BAD_REQUEST);
+            return false;
         }
         Song song = new Song();
         if(map.get("text") != null){
@@ -31,10 +35,92 @@ public class SongService {
         songRepository.save(song);
         if(songRepository.findById(song.getId()).isPresent()){
             System.out.println("POST Request successful: Song created");
-            return new ResponseEntity<>("Creation successful", HttpStatus.CREATED);
+            return true;
         }
         System.out.println("POST Request FAILED");
-        return new ResponseEntity<>("Creation failed, check logs", HttpStatus.BAD_REQUEST);
+        return false;
     }
-
+    public boolean deleteSong(String songId){
+        if(songId == null){
+            throw new IllegalArgumentException("songId must not be null");
+        }
+        if(!songRepository.findById(songId).isPresent()){
+            System.out.println("DELETE Request FAILED: No song with the given songId in the repository");
+            return false;
+        }
+        songRepository.delete(songRepository.findById(songId).get());
+        if(songRepository.findById(songId).isPresent()){
+            System.out.println("DELETE Request FAILED");
+            return false;
+        }
+        return true;
+    }
+    public boolean updateSong(Song song){
+        if(song == null){
+            throw new IllegalArgumentException("Song must not be null");
+        }
+        if(song.getId() == null){
+            throw new IllegalArgumentException("Song ID must not be null");
+        }
+        if(!songRepository.findById(song.getId()).isPresent()){
+            System.out.println("PUT Request FAILED: No song with the given songId in the repository");
+            return false;
+        }
+        Song updSong = songRepository.findById(song.getId()).get();
+        if(song.getStaveList() != null && !song.getStaveList().isEmpty()){
+            updSong.setStaveList(song.getStaveList());
+        }
+        if(song.getName() != null && !song.getName().equals(updSong.getName())){
+            updSong.setName(song.getName());
+        }
+        try{
+            songRepository.save(updSong);
+        }
+        catch (Exception e){
+            System.out.println("PUT Request FAILED");
+            e.printStackTrace();
+            return false;
+        }
+        System.out.println("PUT Request SUCCESSFUL: Song " + updSong.getId() + " updated");
+        return true;
+    }
+    public ArrayList<Song> getAllSongs(){
+        try{
+            ArrayList<Song> songList = new ArrayList<>();
+            songList.addAll(songRepository.findAll());
+            System.out.println("GET Request successful");
+            return songList;
+        }
+        catch (Exception e){
+            System.out.println("GET Request FAILED");
+            throw e;
+        }
+    }
+    public Song getSong(String songId){
+        if(songId == null){
+            throw new IllegalArgumentException("songId must not be null");
+        }
+        if(!songRepository.findById(songId).isPresent()){
+            System.out.println("GET Request FAILED: No song with the given songId in the repository");
+            throw new IllegalArgumentException("No song with the given songId in the repository");
+        }
+        Song song = songRepository.findById(songId).get();
+        System.out.println("GET Request successful");
+        return song;
+    }
+    public ArrayList<Song> getSongsByName(String name){
+        if(name == null){
+            throw new IllegalArgumentException("Name must not be null");
+        }
+        try{
+            ArrayList<Song> songList = new ArrayList<>();
+            songList.addAll(songRepository.findByName(name));
+            System.out.println("GET Request successful");
+            return songList;
+        }
+        catch (Exception e){
+            System.out.println("GET Request FAILED");
+            throw e;
+        }
+    }
 }
